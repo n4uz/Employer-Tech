219569cd7d8de818c6d82e2d32a245d328217feb
n4uz <yorranytoka@gmail.com>
2022-06-13 21:04:49 -0300

    algumas alterações

--- cadastro/processa.php
+++ cadastro/processa.php
@@ -19,9 +19,10 @@ VALUES ('$nome', '$email', '$cpf', '$senha', '$localidade', NOW())";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 if(mysqli_insert_id($conn)) {
-    $_SESSION['msg'] = "Usuário cadastrado com sucesso ";
+    $_SESSION['msg'] = "<p style = 'color: green'> Usuário cadastrado com sucesso </p> ";
     header("Location: index.php");
 }else{
+    $_SESSION['msg'] = "<p style = 'color: red'> Usuário não foi cadastrado com sucesso </p> ";
     header("Location: index.php");
 }
 
