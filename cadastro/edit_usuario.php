a2910d94d4aa1bfa419b8d0e7fb0adacb364a3c4
n4uz <yorranytoka@gmail.com>
2022-06-16 08:37:48 -0300

    algumas alterações

---
+++ cadastro/edit_usuario.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include_once("conexao.php");
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$result_usuario = "SELECT * FROM usuarios WHERE id = '$id'";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+$row_usuario = mysqli_fetch_assoc($resultado_usuario);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>CRUD - Editar</title>		
+	</head>
+	<body>
+		<a href="cad_usuario.php">Cadastrar</a><br>
+		<a href="index.php">Listar</a><br>
+		<h1>Editar Usuário</h1>
+		<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		}
+		?>
+		<form method="POST" action="proc_edit_usuario.php">
+			<input type="hidden" name="id" value="<?php echo $row_usuario['id']; ?>">
+			
+			<label>Nome: </label>
+			<input type="text" name="nome" placeholder="Digite o nome completo" value="<?php echo $row_usuario['nome']; ?>"><br><br>
+			
+			<label>E-mail: </label>
+			<input type="email" name="email" placeholder="Digite o seu melhor e-mail" value="<?php echo $row_usuario['email']; ?>"><br><br>
+
+			<label>CPF: </label>
+			<input type="email" name="cpf" placeholder="Digite o seu CPF" value="<?php echo $row_usuario['cpf']; ?>"><br><br>
+
+			<label>Senha: </label>
+			<input type="email" name="senha" placeholder="Digite uma senha" value="<?php echo $row_usuario['senha']; ?>"><br><br>
+
+			<label>Localidade: </label>
+			<input type="email" name="localidade" placeholder="Escolha uma lugar em que mais se aproxima da sua localidade" value="<?php echo $row_usuario['localidade']; ?>"><br><br>
+			
+			<input type="submit" value="Editar">
+		</form>
+	</body>
+</html>
---
+++ cadastro/proc_edit_usuario.php
@@ -0,0 +1,28 @@
+'<?php
+session_start();
+include_once("conexao.php");
+
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+$localidade = filter_input(INPUT_POST, 'localidade', FILTER_SANITIZE_STRING);   
+
+
+echo "Nome: $nome <br>";
+echo "E-mail: $email <br>";
+echo "Cpf: $cpf <br>";
+echo "Senha: $senha <br>";
+echo "Localidade: $localidade <br>";
+
+$result_usuario = "UPDATE tbldados SET nome='$nome', email='$email', cpf='$cpf', senha='$senha', localidade='$localidade', modified=NOW() WHERE id='$id'";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if(mysqli_affected_rows($conn)){
+	$_SESSION['msg'] = "<p style='color:green;'>Usuário editado com sucesso</p>";
+	header("Location: index.php");
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi editado com sucesso</p>";
+	header("Location: editar.php?id=$id");
+}
